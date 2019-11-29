09cfc0aecd60f2ad8caf99f585a7b73510aa69d1
adam.palmkvist1@outlook.com <adam.palmkvist1@outlook.com>
2019-11-29 23:30:39 +0100

    Adding more comments

---
+++ cmake-build-debug/CMakeFiles/hotel.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/hotel/LoginScreen.cpp" "C:/hotel/cmake-build-debug/CMakeFiles/hotel.dir/LoginScreen.cpp.obj"
+  "C:/hotel/Navigation.cpp" "C:/hotel/cmake-build-debug/CMakeFiles/hotel.dir/Navigation.cpp.obj"
+  "C:/hotel/createAccount.cpp" "C:/hotel/cmake-build-debug/CMakeFiles/hotel.dir/createAccount.cpp.obj"
+  "C:/hotel/main.cpp" "C:/hotel/cmake-build-debug/CMakeFiles/hotel.dir/main.cpp.obj"
+  "C:/hotel/menu.cpp" "C:/hotel/cmake-build-debug/CMakeFiles/hotel.dir/menu.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/hotel.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hotel.dir/LoginScreen.cpp.obj"
+  "CMakeFiles/hotel.dir/Navigation.cpp.obj"
+  "CMakeFiles/hotel.dir/createAccount.cpp.obj"
+  "CMakeFiles/hotel.dir/main.cpp.obj"
+  "CMakeFiles/hotel.dir/menu.cpp.obj"
+  "hotel.exe"
+  "hotel.exe.manifest"
+  "hotel.pdb"
+  "libhotel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hotel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
